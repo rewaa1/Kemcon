6f67d8d313c844f7b20719b198499eb2c755c20e
Rewaa Yasser <142256963+rewaa1@users.noreply.github.com>
2026-05-12 18:42:17 +0300

    feat: add llms.txt context file and implement HomeSections layout component

--- src/app/[locale]/home-sections.tsx
+++ src/app/[locale]/home-sections.tsx
@@ -1,41 +1,16 @@
-"use client";
-
-import dynamic from "next/dynamic";
-import { LazySection } from "@/components/shared/LazySection";
-
-const AboutPreview = dynamic(
-  () => import("@/components/sections/AboutPreview").then((m) => ({ default: m.AboutPreview })),
-  { ssr: false }
-);
-const ProductHighlights = dynamic(
-  () => import("@/components/sections/ProductHighlights").then((m) => ({ default: m.ProductHighlights })),
-  { ssr: false }
-);
-const WhyKemcon = dynamic(
-  () => import("@/components/sections/WhyKemcon").then((m) => ({ default: m.WhyKemcon })),
-  { ssr: false }
-);
-const CTABanner = dynamic(
-  () => import("@/components/sections/CTABanner").then((m) => ({ default: m.CTABanner })),
-  { ssr: false }
-);
+import { AboutPreview } from "@/components/sections/AboutPreview";
+import { ProductHighlights } from "@/components/sections/ProductHighlights";
+import { WhyKemcon } from "@/components/sections/WhyKemcon";
+import { CTABanner } from "@/components/sections/CTABanner";
 
 export function HomeSections({ afterWhyKemcon }: { afterWhyKemcon?: React.ReactNode }) {
   return (
     <>
-      <LazySection>
-        <AboutPreview />
-      </LazySection>
-      <LazySection>
-        <ProductHighlights />
-      </LazySection>
-      <LazySection>
-        <WhyKemcon />
-      </LazySection>
+      <AboutPreview />
+      <ProductHighlights />
+      <WhyKemcon />
       {afterWhyKemcon}
-      <LazySection rootMargin="200px">
-        <CTABanner />
-      </LazySection>
+      <CTABanner />
     </>
   );
 }
